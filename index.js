1d0d18c2d88516e40db9b79e8364a3cdfaafdfe7
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-01-14 13:28:46 +0100

    cache cwd folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const resolveFrom = require('resolve-from');
+const cwd = process.cwd()
 
-module.exports = moduleId => resolveFrom(process.cwd(), moduleId);
-module.exports.silent = moduleId => resolveFrom.silent(process.cwd(), moduleId);
+module.exports = moduleId => resolveFrom(cwd, moduleId);
+module.exports.silent = moduleId => resolveFrom.silent(cwd, moduleId);
