5e061fcb24bc88c7da5a4b273ae616f7932899cc
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-30 19:51:22 +0700

    Meta tweaks

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,8 @@ declare const resolveCwd: {
 	Resolve the path of a module like [`require.resolve()`](https://nodejs.org/api/globals.html#globals_require_resolve) but from the current working directory.
 
 	@param moduleId - What you would use in `require()`.
-	@returns The resolved module path. Throws when the module can't be found.
+	@returns The resolved module path.
+	@throws When the module can't be found.
 
 	@example
 	```
@@ -26,6 +27,20 @@ declare const resolveCwd: {
 
 	@param moduleId - What you would use in `require()`.
 	@returns The resolved module path. Returns `undefined` instead of throwing when the module can't be found.
+
+	@example
+	```
+	import resolveCwd = require('resolve-cwd');
+
+	console.log(__dirname);
+	//=> '/Users/sindresorhus/rainbow'
+
+	console.log(process.cwd());
+	//=> '/Users/sindresorhus/unicorn'
+
+	console.log(resolveCwd.silent('./foo'));
+	//=> '/Users/sindresorhus/unicorn/foo.js'
+	```
 	*/
 	silent(moduleId: string): string | undefined;
 };
